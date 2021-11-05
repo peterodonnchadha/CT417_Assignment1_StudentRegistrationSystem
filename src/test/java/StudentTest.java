4d8e6c10fbcee7998ef0c30a8caa9b9af707c1b4
Peter Donohue <peterdonohue117@gmail.com>
2021-11-05 15:45:32 +0000

    Added jenkinsfile

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package StudentRegistrationSystem;
+//package StudentRegistrationSystem;
 
+import StudentRegistrationSystem.CourseProgramme;
+import StudentRegistrationSystem.Student;
+import StudentRegistrationSystem.Module;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
